60fa00d49dab3d05e452baffa26f5d3fbb3320fc
Ben Harris <benharrislaptop@Bens-MacBook-Pro.local>
2019-12-28 19:38:02 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,17 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+function checkForMatch(){
+	return cardsInPlay[0] === cardsInPlay[1] ? console.log("You found a match!") : console.log("Sorry, try again.");
+}
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2){
+		checkForMatch();
 	}
 }
+
+flipCard(0);
+flipCard(2);
